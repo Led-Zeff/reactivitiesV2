e35fab3ef2711f81fed5dcb96d30709c4dcb46d1
Led-Zeff <ortega.victor.t.v@gmail.com>
2021-02-22 15:33:41 -0600

    Edit and create activities

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -20,5 +20,17 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query{ Id = id });
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command{ Activity = activity }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            return Ok(await Mediator.Send(new Edit.Command{ Id = id, Activity = activity }));
+        }
     }
 }
---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,37 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public System.Guid Id { get; set; }
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                this._context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                activity.Title = request.Activity.Title ?? activity.Title;
+
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+            }
+        }
+    }
+}
