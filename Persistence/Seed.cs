b616d2d7b591906488dfed5655b36410c1e41049
Led-Zeff <ortega.victor.t.v@gmail.com>
2021-03-16 13:15:46 -0600

    Creating auth token

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context) {
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager) {
+            if (!userManager.Users.Any()) {
+                var users = new List<AppUser>
+                {
+                    new AppUser{DisplayName = "Bob", UserName = "bob", Email = "bob@bob.bob"},
+                    new AppUser{DisplayName = "Tom", UserName = "tom", Email = "tom@tom.tom"},
+                    new AppUser{DisplayName = "John", UserName = "john", Email = "john@john.john"}
+                };
+
+                foreach (var item in users)
+                {
+                    await userManager.CreateAsync(item, "Pa$$w0rd");
+                }
+            }
+
             if (context.Activities.Any()) return;
 
             var activities = new List<Activity>
@@ -18,7 +33,7 @@ namespace Persistence
                     Title = "Activity 1",
                     Date = DateTime.Now.AddMonths(-2),
                     Description = "Desc 1",
-                    Category = "Category 1",
+                    Category = "drinks",
                     City = "City 1",
                     Venue = "Venue 1"
                 },
@@ -27,7 +42,7 @@ namespace Persistence
                     Title = "Activity 2",
                     Date = DateTime.Now.AddMonths(-1),
                     Description = "Desc 2",
-                    Category = "Category 2",
+                    Category = "food",
                     City = "City 2",
                     Venue = "Venue 2"
                 },
@@ -36,7 +51,7 @@ namespace Persistence
                     Title = "Activity 3",
                     Date = DateTime.Now.AddDays(-2),
                     Description = "Desc 3",
-                    Category = "Category 3",
+                    Category = "travel",
                     City = "City 3",
                     Venue = "Venue 3"
                 }
