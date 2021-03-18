e7c9339766f06d982e154600d67bbfa8b0533951
Led-Zeff <ortega.victor.t.v@gmail.com>
2021-03-17 18:15:09 -0600

    Send validation errors

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -33,7 +33,7 @@ namespace API.Controllers
             if (user == null) return Unauthorized();
 
             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
-            if (result.Succeeded) return Unauthorized();
+            if (!result.Succeeded) return Unauthorized();
 
             return CreateUserDto(user);
         }
@@ -43,12 +43,14 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email taken");
+                ModelState.AddModelError("email", "Email taken");
+                return ValidationProblem(ModelState);
             }
 
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
             {
-                return BadRequest("Username taken");
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem(ModelState);
             }
 
             var user = new AppUser
@@ -61,7 +63,7 @@ namespace API.Controllers
             var result = await _userManager.CreateAsync(user, registerDto.Password);
 
             if (!result.Succeeded) return BadRequest("Problem registering user");
-            
+
             return CreateUserDto(user);
         }
 
